48d8f486c484e8a6f3a3e6f2505a9046d8dc04e3
bakaur21 <bakaur.ext@deloitte.com>
2023-09-26 17:10:08 +0530

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,19 @@
+def parse_string(stri):
+    parts = stri.split('0')
+    parts = [part for part in parts if part]
+    if len(parts) >= 3:
+        f_name = parts[0]
+        l_name = parts[1]
+        id_value = parts[2]
+        result = {
+            "first_name": f_name,
+            "last_name": l_name,
+            "id": id_value
+        }
+        return result
+    else:
+        return None
+
+encoded_str = "Baljeet000kaur0002112"
+parsed_data = parse_string(stri)
+print(parsed_data)
